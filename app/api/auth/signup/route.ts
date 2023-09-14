17fa8ae4991f17abe56d3d9c67676d312c0db89a
Bozos2 <bozesoldo8@gmail.com>
2023-09-14 15:42:59 +0200

    auth updated

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,64 +1,87 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { hash } from "bcryptjs";
 import { connectToMongoDB } from "@/app/lib/mongodb";
 import { RUser } from "@/app/types";
 import mongoose from "mongoose";
 import User from "@/app/models/user";
-import { NextApiRequest, NextApiResponse } from "next";
+interface BodyResponseProp {
+  email: string;
+  fullName: string;
+  password: string;
+}
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  connectToMongoDB().catch((err) => res.json(err));
+export async function POST(req: NextRequest, res: NextResponse) {
+  connectToMongoDB().catch((err) =>
+    NextResponse.json({ err }, { status: 500 })
+  );
+  const data: BodyResponseProp = await req.json();
 
   if (req.method === "POST") {
-    if (!req.body) return res.status(400).json({ error: "Data is missing" });
+    if (!req.body)
+      return NextResponse.json({ error: "Data is missing" }, { status: 400 });
 
-    const data = res.json;
     console.log(data);
 
-    const { email, fullName, password } = req.body;
+    const { email, fullName, password } = data;
 
-    const userExists = await User.findOne({ email });
+    try {
+      const userExists = await User.findOne({ email });
 
-    if (userExists) {
-      return res.status(409).json({ error: "User Already exists" });
-    } else {
-      if (password.length < 6)
-        return res
-          .status(409)
-          .json({ error: "Password should be 6 characters long" });
+      if (userExists) {
+        return NextResponse.json(
+          { error: "User Already exists" },
+          { status: 409 }
+        );
+      } else {
+        if (password.length < 5)
+          return NextResponse.json(
+            { error: "Password should be 5 characters long" },
+            { status: 409 }
+          );
 
-      const hashedPassword = await hash(password, 12);
+        const hashedPassword = await hash(password, 12);
 
-      try {
-        await User.create(
-          {
+        try {
+          const data: RUser = await User.create({
             fullName,
             email,
             password: hashedPassword,
-          },
-          (error: unknown, data: RUser) => {
-            if (error && error instanceof mongoose.Error.ValidationError) {
-              for (let field in error.errors) {
-                const msg = error.errors[field].message;
-                return res.status(409).json({ error: msg });
-              }
-            }
+          });
 
-            const user = {
-              email: data?.email,
-              fullName: data?.fullName,
-              _id: data?._id,
-            };
+          const user = {
+            email: data.email,
+            fullName: data.fullName,
+            _id: data._id,
+          };
 
-            return res.status(201).json({
+          return NextResponse.json(
+            {
               success: true,
               user,
-            });
+            },
+            { status: 201 }
+          );
+        } catch (error) {
+          if (error instanceof mongoose.Error.ValidationError) {
+            const errors = Object.values(error.errors).map(
+              (err) => err.message
+            );
+            return NextResponse.json(
+              { errors },
+              {
+                status: 403,
+              }
+            );
           }
-        );
-      } catch {
-        return res.status(400).json({ error: "problem with user create" });
+
+          return NextResponse.json(
+            { error: "Problem with user create" },
+            { status: 400 }
+          );
+        }
       }
+    } catch (error) {
+      return NextResponse.json({ error }, { status: 400 });
     }
   } else {
     return console.log("method not allowed");
